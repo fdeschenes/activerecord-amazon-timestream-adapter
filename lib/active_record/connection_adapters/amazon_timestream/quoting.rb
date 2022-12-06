680576b94e2754abcc55dd0a1619fb25cda6917d
krishnaprasad <kp2222@gmail.com>
2022-12-06 13:32:22 +0530

    do not include database name when table does not exist in database

--- lib/active_record/connection_adapters/amazon_timestream/quoting.rb
+++ lib/active_record/connection_adapters/amazon_timestream/quoting.rb
@@ -5,7 +5,11 @@ module ActiveRecord
     module AmazonTimestream
       module Quoting
         def quote_table_name(table_name)
-          "\"#{@database}\".\"#{quote_string(table_name)}\""
+          if table_exists?(table_name)
+            "\"#{@database}\".\"#{quote_string(table_name)}\""
+          else
+            "\"#{quote_string(table_name)}\""
+          end
         end
       end
     end
