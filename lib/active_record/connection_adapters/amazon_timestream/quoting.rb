29800f8744348813c94acbc2d3c49229ef7d31d0
Francois Deschenes <francois@tintup.com>
2024-08-16 20:25:57 +0100

    Adds missing column quoting for Rails 7.2+

--- lib/active_record/connection_adapters/amazon_timestream/quoting.rb
+++ lib/active_record/connection_adapters/amazon_timestream/quoting.rb
@@ -2,6 +2,10 @@ module ActiveRecord
   module ConnectionAdapters
     module AmazonTimestream
       module Quoting
+        def quote_column_name(column_name)
+          column_name.to_s
+        end
+
         def quote_table_name(table_name)
           database = ActiveRecord::VERSION::STRING >= '7.1.0' ? @config[:database] : @database
           "\"#{database}\".#{quote_table_name_without_database(table_name)}"
