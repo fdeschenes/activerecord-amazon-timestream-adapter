8aca0d8e7de64e53bdf7815e59160b7109d04fb8
Francois Deschenes <francois@tintup.com>
2024-08-17 12:55:09 +0100

    More fixes to quoting for ActiveRecord 7.2

--- lib/active_record/connection_adapters/amazon_timestream/quoting.rb
+++ lib/active_record/connection_adapters/amazon_timestream/quoting.rb
@@ -5,31 +5,20 @@ module ActiveRecord
         if ActiveRecord::VERSION::STRING >= '7.2.0'
           extend ActiveSupport::Concern
 
-          if ActiveRecord::VERSION::STRING >= '7.2.0'
-            module ClassMethods
-              def quote_column_name(column_name)
-                column_name.to_s
-              end
-
-              def quote_table_name(table_name)
-                database = @config[:database]
-                "\"#{database}\".#{quote_table_name_without_database(table_name)}"
-              end
-
-              def quote_table_name_without_database(table_name)
-                "\"#{quote_string(table_name)}\""
-              end
+          module ClassMethods
+            def quote_column_name(column_name)
+              column_name.to_s
             end
           end
-        else
-          def quote_table_name(table_name)
-            database = ActiveRecord::VERSION::STRING >= '7.1.0' ? @config[:database] : @database
-            "\"#{database}\".#{quote_table_name_without_database(table_name)}"
-          end
+        end
 
-          def quote_table_name_without_database(table_name)
-            "\"#{quote_string(table_name)}\""
-          end
+        def quote_table_name(table_name)
+          database = ActiveRecord::VERSION::STRING >= '7.1.0' ? @config[:database] : @database
+          "\"#{database}\".#{quote_table_name_without_database(table_name)}"
+        end
+
+        def quote_table_name_without_database(table_name)
+          "\"#{quote_string(table_name)}\""
         end
       end
     end
